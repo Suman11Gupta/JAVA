c1dde7f833c8c923dbd4c01f926964390de46edf
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-06-22 19:16:00 +0530

    Update Password_Checker1.java

--- Password_Checker1.java
+++ Password_Checker1.java
@@ -37,5 +37,45 @@ public class Password_Checker1 {
                 }
             }
         }
+
+         int selectedAnswer;
+        System.out.println("1. Who invented Java Programming?");
+        System.out.println(" 1. ) Guido van Rossum");
+        System.out.println(" 2. ) James Gosling");
+        System.out.println(" 3. ) Dennis Ritchie");
+        System.out.println(" 4. ) Bjarne Stroustrup");
+        System.out.println("Select your answer: ");
+
+        selectedAnswer=sc.nextInt();
+
+        switch(selectedAnswer){
+            case 2:
+                System.out.println(" select Right Answer ");
+                System.out.println(" 2. Which component is used to compile, debug and execute the java programs?");
+                System.out.println(" 1. ) JRE");
+                System.out.println(" 2. ) JIT");
+                System.out.println(" 3. ) JDK");
+                System.out.println(" 4. ) JVM");
+                System.out.println("Select your answer: ");
+                selectedAnswer=sc.nextInt();
+
+                switch(selectedAnswer){
+                    case 3:
+                        System.out.println(" select Right Answer ");
+                        break;
+
+                    default:
+                        System.out.println(" Try next year... ");
+                        break;
+                }
+                break;
+
+
+
+            default:
+                System.out.println("Try next year...");
+                break;
+            }
+
        }
     }
