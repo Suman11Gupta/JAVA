c324c9035fb3111ec676f3396f0d2a488fb81eed
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-06-23 20:32:05 +0530

    Update Password_Checker1.java

--- Password_Checker1.java
+++ Password_Checker1.java
@@ -3,75 +3,168 @@ import java.util.Scanner;
 
 public class Password_Checker1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String setPassword, enterPassword;
-        System.out.println("Set your password: ");
-        setPassword = sc.nextLine();
-        System.out.println("Enter your password: ");
-        enterPassword = sc.nextLine();
+        Scanner sc=new Scanner(System.in);
+        String setPassword,enterPassword;
+        System.out.println("Set Your Password: ");
+        setPassword=sc.nextLine();
+        System.out.println("Enter Your Password: ");
+        enterPassword=sc.nextLine();
+        int selectedAnswer;
 
-        if (setPassword.equals(enterPassword)) {
-            System.out.println("correct");
-        } else if (!setPassword.equals(enterPassword)) {
-            System.out.println("wrong password ... try 2 more time out of 2");
-            System.out.println("Enter your password: ");
-            enterPassword = sc.nextLine();
-            if (setPassword.equals(enterPassword)) {
-                System.out.println("correct");
-            } else {
-                System.out.println("wrong password ... try 1 more time out of 1");
-                System.out.println("Enter your password: ");
-                enterPassword = sc.nextLine();
-                if (setPassword.equals(enterPassword)) {
-                    System.out.println("correct");
-                } else {
-                    System.out.println("wrong password ... try 0 more time out of 0");
-                    System.out.println("Enter your password: ");
-                    enterPassword = sc.nextLine();
+        if(setPassword.equals(enterPassword)){
+            System.out.println("Your password is correct...");
 
-                    if (setPassword.equals(enterPassword)) {
-                        System.out.println("correct");
-                    } else {
-                        System.out.println("sorry your time is out \n");
+            System.out.println("1. Who invented Java Programming?");
+            System.out.println("1. ) Guido van Rossum");
+            System.out.println("2. ) James Gosling");
+            System.out.println("3. ) Dennis Ritchie");
+            System.out.println("4. ) Bjarne Stroustru");
+            System.out.println("Select Answer: ");
+            selectedAnswer=sc.nextInt();
+            switch (selectedAnswer){
+                case 2:
+                    System.out.println("Your answer is correct...");
+                    System.out.println("2. Which component is used to compile, debug and execute the java program?");
+                    System.out.println("1. ) JRE");
+                    System.out.println("2. ) JIT");
+                    System.out.println("3. ) JDK");
+                    System.out.println("4. ) JVM");
+                    System.out.println("Select your answer: ");
+                    selectedAnswer=sc.nextInt();
+                    switch(selectedAnswer){
+                        case 3:
+                            System.out.println("your answer is correct");
+                            break;
+                        default:
+                            System.out.println("Your answer is incorrect try next year...");
+                            break;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Your answer is incorrect try next year...");
+                    break;
+            }
+        }else if(!setPassword.equals(enterPassword)){
+            System.out.println("Password is in correct try 3 times");
+            System.out.println("Enter Your Password: ");
+            enterPassword=sc.nextLine();
+            if(setPassword.equals(enterPassword)){
+                System.out.println("Your password is correct...");
+
+                System.out.println("1. Who invented Java Programming?");
+                System.out.println("1. ) Guido van Rossum");
+                System.out.println("2. ) James Gosling");
+                System.out.println("3. ) Dennis Ritchie");
+                System.out.println("4. ) Bjarne Stroustru");
+                System.out.println("Select Answer: ");
+                selectedAnswer=sc.nextInt();
+                switch (selectedAnswer){
+                    case 2:
+                        System.out.println("Your answer is correct...");
+                        System.out.println("2. Which component is used to compile, debug and execute the java program?");
+                        System.out.println("1. ) JRE");
+                        System.out.println("2. ) JIT");
+                        System.out.println("3. ) JDK");
+                        System.out.println("4. ) JVM");
+                        System.out.println("Select your answer: ");
+                        selectedAnswer=sc.nextInt();
+                        switch(selectedAnswer){
+                            case 3:
+                                System.out.println("your answer is correct");
+                                break;
+                            default:
+                                System.out.println("Your answer is incorrect try next year...");
+                                break;
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Your answer is incorrect try next year...");
+                        break;
+                }
+            }else{
+                System.out.println("Password is in correct try 2 times");
+                System.out.println("Enter Your Password: ");
+                enterPassword=sc.nextLine();
+                if(setPassword.equals(enterPassword)){
+                    System.out.println("Your password is correct...");
+
+                    System.out.println("1. Who invented Java Programming?");
+                    System.out.println("1. ) Guido van Rossum");
+                    System.out.println("2. ) James Gosling");
+                    System.out.println("3. ) Dennis Ritchie");
+                    System.out.println("4. ) Bjarne Stroustru");
+                    System.out.println("Select Answer: ");
+                    selectedAnswer=sc.nextInt();
+                    switch (selectedAnswer){
+                        case 3:
+                            System.out.println("Your answer is correct...");
+                            System.out.println("2. Which component is used to compile, debug and execute the java program?");
+                            System.out.println("1. ) JRE");
+                            System.out.println("2. ) JIT");
+                            System.out.println("3. ) JDK");
+                            System.out.println("4. ) JVM");
+                            System.out.println("Select your answer: ");
+                            selectedAnswer=sc.nextInt();
+                            switch(selectedAnswer){
+                                case 3:
+                                    System.out.println("your answer is correct");
+                                    break;
+                                default:
+                                    System.out.println("Your answer is incorrect try next year...");
+                                    break;
+                            }
+                            break;
+
+                        default:
+                            System.out.println("Your answer is incorrect try next year...");
+                            break;
+                    }
+                }else{
+                    System.out.println("Password is in correct try 1 times");
+                    System.out.println("Enter Your Password: ");
+                    enterPassword=sc.nextLine();
+                    if(setPassword.equals(enterPassword)){
+                        System.out.println("Your password is correct...");
+
+                        System.out.println("1. Who invented Java Programming?");
+                        System.out.println("1. ) Guido van Rossum");
+                        System.out.println("2. ) James Gosling");
+                        System.out.println("3. ) Dennis Ritchie");
+                        System.out.println("4. ) Bjarne Stroustru");
+                        System.out.println("Select Answer: ");
+                        selectedAnswer=sc.nextInt();
+                        switch (selectedAnswer){
+                            case 3:
+                                System.out.println("Your answer is correct...");
+                                System.out.println("2. Which component is used to compile, debug and execute the java program?");
+                                System.out.println("1. ) JRE");
+                                System.out.println("2. ) JIT");
+                                System.out.println("3. ) JDK");
+                                System.out.println("4. ) JVM");
+                                System.out.println("Select your answer: ");
+                                selectedAnswer=sc.nextInt();
+                                switch(selectedAnswer){
+                                    case 3:
+                                        System.out.println("your answer is correct");
+                                        break;
+                                    default:
+                                        System.out.println("Your answer is incorrect try next year...");
+                                        break;
+                                }
+                                break;
+
+                            default:
+                                System.out.println("Your answer is incorrect try next year...");
+                                break;
+                        }
+                    }else{
+                        System.out.println("Time out...");
                     }
                 }
             }
         }
-
-         int selectedAnswer;
-        System.out.println("1. Who invented Java Programming?");
-        System.out.println(" 1. ) Guido van Rossum");
-        System.out.println(" 2. ) James Gosling");
-        System.out.println(" 3. ) Dennis Ritchie");
-        System.out.println(" 4. ) Bjarne Stroustrup");
-        System.out.println("Select your answer: ");
-        selectedAnswer=sc.nextInt();
-
-        switch(selectedAnswer){
-            case 2:
-                System.out.println(" select Right Answer ");
-                System.out.println(" 2. Which component is used to compile, debug and execute the java programs?");
-                System.out.println(" 1. ) JRE");
-                System.out.println(" 2. ) JIT");
-                System.out.println(" 3. ) JDK");
-                System.out.println(" 4. ) JVM");
-                System.out.println("Select your answer: ");
-                selectedAnswer=sc.nextInt();
-              
-                switch(selectedAnswer){
-                    case 3:
-                        System.out.println(" select Right Answer ");
-                        break;
-                    default:
-                        System.out.println(" Try next year... ");
-                        break;
-                }
-                
-                break;
-                default:
-                System.out.println("Try next year...");
-                break;
-            }
 
        }
     }
