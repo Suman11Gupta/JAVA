5a009742a3ed3554093c6ddb11a1a0454094fafb
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-06-25 20:19:21 +0530

    Update Salary_System.java

--- Salary_System.java
+++ Salary_System.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Salary_System {
     public static void main(String[] args) {
         String employeeId,employeeName;
-        int month,year,leaves,salaryBasic,yearlySalary,paidDays;
-        float conveyanceAllowance,medicalAllowance,otherAllowances,professionalTax;
+        int month,year,leaves,paidDays,totalWorkingDays;
+        float salaryBasic,yearlySalary,conveyanceAllowance,medicalAllowance,otherAllowances,professionalTax,leaveAmount;
         Scanner sc=new Scanner(System.in);
 
         System.out.println("Employee ID: ");
@@ -16,6 +16,8 @@ public class Salary_System {
         month=sc.nextInt();
         System.out.println("year: ");
         year=sc.nextInt();
+        System.out.println("Total Working Days: ");
+        totalWorkingDays=sc.nextInt();
         System.out.println("Leaves: ");
         leaves=sc.nextInt();
         System.out.println("Conveyance Allowance: ");
@@ -25,17 +27,15 @@ public class Salary_System {
         System.out.println("Other Allowances: ");
         otherAllowances=sc.nextFloat();
         System.out.println("Salary Basic: ");
-        salaryBasic=sc.nextInt();
+        salaryBasic=sc.nextFloat();
         System.out.println("Professional Tax: ");
         professionalTax=sc.nextFloat();
 
         yearlySalary=salaryBasic*12;
-        System.out.println("Paid Days: ");
-        paidDays=sc.nextInt();
+        System.out.println("Paid Days: "+(totalWorkingDays-leaves));
         System.out.println("Basic Salary: "+salaryBasic);
+        leaveAmount=(salaryBasic/totalWorkingDays)*leaves;
+        System.out.println("Leaves Amount: "+leaveAmount);
         
-
-
-
     }
 }
