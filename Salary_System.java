764fdae55fc58e64c08a748587f29f7a054060d8
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-02 18:45:58 +0530

    Update Salary_System.java

--- Salary_System.java
+++ Salary_System.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Salary_System {
     public static void main(String[] args) {
-        String employeeId,employeeName;
+          String employeeId,employeeName;
         int month,year,leaves,paidDays,totalWorkingDays;
         float salaryBasic,yearlySalary,conveyanceAllowance,medicalAllowance,otherAllowances,professionalTax,leaveAmount,CTC,totalAllowance,totalDeduction,
         totalAllowancesMonthly,netSalary;
@@ -13,10 +13,57 @@ public class Salary_System {
         employeeId=sc.nextLine();
         System.out.println("Employee Name: ");
         employeeName=sc.nextLine();
-        System.out.println("Month: ");
+        System.out.println("Select Month: ");
         month=sc.nextInt();
+        switch(month){
+                case 1:
+                System.out.println("January");
+                break;
+                case 2:
+                System.out.println("February");
+                break;
+                case 3:
+                System.out.println("March");
+                break;
+                case 4:
+                System.out.println("April");
+                break;
+                case 5:
+                System.out.println("May");
+                break;
+                case 6:
+                System.out.println("June");
+                break;
+                case 7:
+                System.out.println("July");
+                break;
+                case 8:
+                System.out.println("August");
+                break;
+                case 9:
+                System.out.println("September");
+                break;
+                case 10:
+                System.out.println("October");
+                break;
+                case 11:
+                System.out.println("November");
+                break;
+                case 12:
+                System.out.println("December");
+                break;
+                default:
+                System.out.println("Please select 1-12");
+                break;
+        }
+
         System.out.println("year: ");
         year=sc.nextInt();
+        if(year%4==0){
+            System.out.println("Leap Year");
+        }else{
+            System.out.println("not leap year");
+        }
         System.out.println("Total Working Days: ");
         totalWorkingDays=sc.nextInt();
         System.out.println("Leaves: ");
@@ -49,9 +96,9 @@ public class Salary_System {
         System.out.println("Total Allowance: "+totalAllowance);
         professionalTax=(yearlySalary*professionalTax/100)-(salaryBasic-leaveAmount);
         System.out.println("Professional Tax: "+professionalTax);
-        totalDeduction=(salaryBasic)-(professionalTax+leaveAmount);
+        totalDeduction=(professionalTax+leaveAmount)-(salaryBasic);
         System.out.println("Total Deduction: "+totalDeduction);
-        totalAllowancesMonthly=((salaryBasic*conveyanceAllowance)/100)+((salaryBasic*medicalAllowance)/100)+((salaryBasic*otherAllowances)/100);
+        totalAllowancesMonthly=(conveyanceAllowance/12)+(medicalAllowance/12)+(otherAllowances/12);
         System.out.println("Total Allowance Monthly: "+totalAllowancesMonthly);
         netSalary=(totalAllowancesMonthly+salaryBasic)-totalDeduction;
         System.out.println("Net Salary: "+netSalary);
