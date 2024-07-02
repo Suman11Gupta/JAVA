1e1aca1041652132acd3b72fb616ed5731965db6
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-02 19:21:31 +0530

    Add files via upload

---
+++ Square_Fill_Pattern.java
@@ -0,0 +1,12 @@
+package Suman;
+
+public class Square_Fill_Pattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
