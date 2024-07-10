f4908cea775e283655435f5957dd6858a26cc6f4
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-10 20:15:09 +0530

    Update Billing_System.java

--- Billing_System.java
+++ Billing_System.java
@@ -3,35 +3,41 @@ import java.util.Scanner;
 public class Billing_System {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-//        int size;
-//        System.out.print("Enter Product List Size: ");
-//        size=sc.nextInt();
-//        sc.nextLine();
-//        String[] product=new String[size];
-//        for(int i=0;i<size;i++){
-//            product[i]=sc.nextLine();
-//            System.out.print("Enter Product "+(i+1)+":");
-//
-//        }
-
 
         int size;
         System.out.print("Enter Product List Size: ");
         size = sc.nextInt();
-        sc.nextLine();
 
         String[] product = new String[size];
 
         for (int i = 0; i < size; i++) {
-            System.out.print("Enter Product " + (i + 1) + ": ");
-            product[i] = sc.nextLine();
+            System.out.println("Enter Product " + (i + 1) + ": ");
+            product[i] = sc.next();
         }
 
-
-        System.out.println("Product list:");
-        for (String item : product) {
-            System.out.println(item);
+        float price[]=new float[size];
+        float total;
+        total=0;
+        for(int i=0;i<size;i++){
+            System.out.println("Enter "+product[i]+" Price:");
+            price[i]=sc.nextInt();
+            total=total+price[i];
         }
+
+        System.out.println("...........Output.............");
+        for(int i=0;i<size;i++){
+            System.out.println((i+1)+". "+product[i]+"= "+price[i]+"/-");
+        }
+
+        System.out.println("..............................");
+        System.out.println("Total ="+total);
+
+
+        
+//        for (int i = 0; i < size; i++) {
+//            System.out.println("Product List: " + (i + 1) + ": "+product[i]);
+//
+//        }
 
     }
 }
