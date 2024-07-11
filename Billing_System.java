447e8eeec43878c5c797c10cd1db8d246852a9b0
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-11 19:30:07 +0530

    Update Billing_System.java

--- Billing_System.java
+++ Billing_System.java
@@ -2,7 +2,7 @@ package Suman;
 import java.util.Scanner;
 public class Billing_System {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+     Scanner sc=new Scanner(System.in);
 
         int size;
         System.out.print("Enter Product List Size: ");
@@ -22,18 +22,51 @@ public class Billing_System {
             System.out.println("Enter "+product[i]+" Price:");
             price[i]=sc.nextInt();
             total=total+price[i];
+
         }
 
-        System.out.println("...........Output.............");
-        for(int i=0;i<size;i++){
-            System.out.println((i+1)+". "+product[i]+"= "+price[i]+"/-");
+        char select;
+        System.out.println("Do You Want to add GST [Y/N]");
+        select=sc.next().charAt(0);
+        switch(select){
+            case 'Y':
+                System.out.println("...........Output.............");
+                for(int i=0;i<size;i++){
+                    System.out.println((i+1)+". "+product[i]+"= "+price[i]+"/-");
+                }
+                float gst=0;
+                System.out.println("..............................");
+                System.out.println("Total ="+total);
+                gst=(18*total)/100;
+                System.out.println("18% GST ="+gst);
+
+                System.out.println("..........................");
+                System.out.println("Final Total ="+(total+gst)+"/-");
+                System.out.println("..........................");
+                break;
+
+            case 'N':
+                System.out.println("...........Output.............");
+                for(int i=0;i<size;i++){
+                    System.out.println((i+1)+". "+product[i]+"= "+price[i]+"/-");
+                }
+                System.out.println("..............................");
+                System.out.println("Total ="+total);
+                break;
+            default:
+                System.out.println("Please select Y/N");
+                break;
         }
 
-        System.out.println("..............................");
-        System.out.println("Total ="+total);
+//        System.out.println("...........Output.............");
+//        for(int i=0;i<size;i++){
+//            System.out.println((i+1)+". "+product[i]+"= "+price[i]+"/-");
+//        }
+//
+//        System.out.println("..............................");
+//        System.out.println("Total ="+total);
 
 
-        
 //        for (int i = 0; i < size; i++) {
 //            System.out.println("Product List: " + (i + 1) + ": "+product[i]);
 //
