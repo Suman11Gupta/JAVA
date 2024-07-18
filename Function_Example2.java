af65afd4f9ec40d59404767246f39cced39e43bf
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-18 19:55:47 +0530

    Add files via upload

---
+++ Function_Example2.java
@@ -0,0 +1,11 @@
+package Suman;
+
+public class Function_Example2 {
+    public static void main(String[] args) {
+        message("Non return type with argument....");
+    }
+    static void message(String message){
+        System.out.println("Message: "+message );
+
+    }
+}
