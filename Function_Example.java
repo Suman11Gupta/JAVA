b627f42910e3191e5e1441a6104d2406d6e002d6
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-07-18 19:55:14 +0530

    Add files via upload

---
+++ Function_Example.java
@@ -0,0 +1,13 @@
+package Suman;
+
+public class Function_Example {
+    public static void main(String[] args) {
+        int result=message(5,6);
+        System.out.println("your result: "+result);
+    }
+    static int message(int a,int b){
+        System.out.println("Hello");
+        int c=a+b;
+        return c;
+    }
+}
