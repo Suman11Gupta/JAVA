0f17b1fc62999db3e58e22daf56b11ce800bcd8d
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-08-26 20:17:39 +0530

    Update Greater_number.java

--- Prac/Greater_number.java
+++ Prac/Greater_number.java
@@ -1,7 +1,22 @@
 package Prac;
 import java.util.Scanner;
 public class Greater_number {
-    int a,b,c;
-    Scanner sc=new Scanner(System.in);
-    
+ public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        a=sc.nextInt();
+        System.out.println("Enter second number: ");
+        b=sc.nextInt();
+        System.out.println("Enter third number: ");
+        c=sc.nextInt();
+
+        if(a>b && a>c){
+            System.out.println("First number is greater");
+        }else if(b>a && b>c){
+            System.out.println("Second number is greater");
+        }else{
+            System.out.println("Third number is greater");
+        }
+    }
 }
