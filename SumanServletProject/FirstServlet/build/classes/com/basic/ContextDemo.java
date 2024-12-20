d5f7531eb3ab71b8a81b7f0a24e7f7520f11200e
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2024-12-20 17:14:12 +0530

    Add files via upload

---
+++ SumanServletProject/FirstServlet/build/classes/com/basic/ContextDemo.java
@@ -0,0 +1,37 @@
+package com.basic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextDemo
+ */
+@WebServlet("/ContextDemo")
+public class ContextDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter obj=response.getWriter();
+		ServletContext context=getServletContext();
+		
+		obj.println("Hello "+context.getInitParameter("uname"));  
+		obj.println("Please Check your mail. mail address is "+context.getInitParameter("email"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
