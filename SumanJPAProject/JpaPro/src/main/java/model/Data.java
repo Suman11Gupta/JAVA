1fc36e8dc840402e3cae6c8cc3916e4032b9a6e3
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2025-01-17 19:02:10 +0530

    Update Data.java

--- SumanJPAProject/JpaPro/src/main/java/model/Data.java
+++ SumanJPAProject/JpaPro/src/main/java/model/Data.java
@@ -8,15 +8,48 @@ public class Data {
 	public static void main(String[] args) {
 		EntityManagerFactory factory=Persistence.createEntityManagerFactory("JpaPro");
 		EntityManager manage=factory.createEntityManager();
+
+		//---------------Insert Record-----------------------
+		// manage.getTransaction().begin();
+		// Employee ob=new Employee();
+		// ob.setDeptno(10);
+		// ob.setEmpno(456);
+		// ob.setName("Suvi");
+		// ob.setSalary(450000);
+		// manage.persist(ob);
+		// manage.getTransaction().commit();
+		// System.out.println("Record save!!!!!!!!!!!");
+
+		//------------------Display All Records------------------
 		
-		manage.getTransaction().begin();
-		Employee ob=new Employee();
-		ob.setDeptno(10);
-		ob.setEmpno(456);
-		ob.setName("Suvi");
-		ob.setSalary(450000);
-		manage.persist(ob);
-		manage.getTransaction().commit();
-		System.out.println("Record save!!!!!!!!!!!");
+//		transaction.begin();
+//		List<Employee> all=manage.createQuery("Select e From Employee e").getResultList();
+//		System.out.println("All Employee");
+//		System.out.println("Deptno\tEmpno\tName\tSalary");
+//		System.out.println("-------------------------------");
+//		for(Employee Ev:all) {
+//			System.out.print(Ev.getDeptno()+"   ");
+//			System.out.print(Ev.getEmpno()+"  ");
+//			System.out.print(Ev.getName()+"  ");
+//			System.out.println(Ev.getSalary());
+//		}
+//		transaction.commit();
+		
+		//--------------------Update Record-----------------------
+		
+//	transaction.begin();
+//	Employee updateob=manage.find(Employee.class, 89);
+//	updateob.setName("Vineeta");
+//	transaction.commit();
+//	System.out.println("Record Updated");
+	
+	//-------------------Delete Record-----------------------------
+	
+	transaction.begin();
+	Employee delob=manage.find(Employee.class, 102);
+	manage.remove(delob);
+	transaction.commit();
+	System.out.println("Record deleted");
+		
 	}
 }
